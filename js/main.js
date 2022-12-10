568a9d04d10257c483133e00af445f6eea3930d4
Basem Gaber <basemgaber01111@gmail.com>
2022-12-10 09:53:29 +0200

    edit front

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@ if (document.querySelector(".main-swiper")) {
       delay: 2500,
       disableOnInteraction: false,
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     pagination: {
       el: ".swiper-pagination",
     },
