02bed335329375f0767ed36aa4f13f9d387d5287
Basem Gaber <basemgaber01111@gmail.com>
2022-12-10 09:03:20 +0200

    edit front

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// Initialize Swiper Slider
-// Set Landing Page Slider
-if (document.querySelector(".landing-swiper")) {
-  const swiperElement = document.querySelector(".landing-swiper")
-  const swiper = new Swiper(swiperElement, {
-    slidesPerView: 1,
-    centeredSlides: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-}
-// Set Main Slider
 if (document.querySelector(".main-swiper")) {
   const swiperElement = document.querySelector(".main-swiper")
   const swiper = new Swiper(swiperElement, {
@@ -31,7 +14,6 @@ if (document.querySelector(".main-swiper")) {
     },
   });
 }
-
 // btn-inp-check
 if (document.querySelector(".btn-inp-check .inp")) {
   let inputsCheck = document.querySelectorAll(".btn-inp-check .inp");
