262ff97a612992b70a56c4e6a8c05ea8d34949b0
unknown <marwan.nasser565@gamil.com>
2022-12-14 11:34:02 +0200

    last update

--- js/main.js
+++ js/main.js
@@ -37,3 +37,7 @@ if(document.querySelector("#popup_show")) {
   let myModal = new bootstrap.Modal(document.getElementById("popup_show"));
   myModal.show();
 }
+
+// initialize bootstrap tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
